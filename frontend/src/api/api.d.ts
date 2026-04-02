868cef3507596a4d43974b051356f05e864b79b2
Victor Gavilan <victormanuelgavilanr18@gmail.com>
2026-04-01 21:59:23 -0300

    fix: typescript build errors

--- frontend/src/api/api.d.ts
+++ frontend/src/api/api.d.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://todo-app-fullstack-jyw8.onrender.com/api/"
+  
 });
 
 api.interceptors.request.use((config) => {
